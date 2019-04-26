b3ea7cf3ba6a3fc4ecaa070de88b2582d397140e
Dan Bryk <danbryk@icloud.com>
2019-04-26 18:21:05 -0400

    bloc jams jquery checkpoint 10 assignment completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -7,14 +7,21 @@
   $('button#next').on('click', function () {
     if (player.playState !== 'playing') { return; }
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex +1 ;
+    const nextSongIndex = currentSongIndex + 1 ;
     if (nextSongIndex >= album.songs.length) { return; }
-    
+
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
 
   $('button#previous').on('click', function () {
+    if (player.playState !== 'playing') { return; }
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const prevSongIndex = currentSongIndex - 1 ;
+    if (prevSongIndex < 0) { return; }
+
+    const prevSong = album.songs[prevSongIndex];
+    player.playPause(prevSong);
   });
 
 }
