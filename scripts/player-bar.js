e0f7c733f2d9a6449b7a6255b7f0324a0e56df3e
Dan Bryk <danbryk@icloud.com>
2019-05-02 17:51:07 -0400

    Checkpoint 11 exercise completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -14,6 +14,10 @@
     player.playPause(nextSong);
   });
 
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
   $('button#previous').on('click', function () {
     if (player.playState !== 'playing') { return; }
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
@@ -23,5 +27,14 @@
     const prevSong = album.songs[prevSongIndex];
     player.playPause(prevSong);
   });
+
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+  }, 1000);
 
 }
